179a63119cf76dc1684c03dc58120719d2b9f958
Joseph Sesti <sestijoey63@gmail.com>
2019-10-23 19:10:12 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -18,3 +18,13 @@ INSERT INTO users VALUES (17, 'Whale', 6);
 INSERT INTO users VALUES (18, 'Pacha', 5);
 INSERT INTO users VALUES (19, 'Ena', 24);
 INSERT INTO users VALUES (20, 'Katie', 24);
+INSERT INTO projects VALUES (1, 10.00, 1, 2),
+INSERT INTO projects VALUES(2, 20.00, 1, 3),
+INSERT INTO projects VALUES(3, 40.00, 1, 4),
+INSERT INTO projects VALUES(4, 50.00, 2, 3),
+INSERT INTO projects VALUES(5, 10.00, 3, 2),
+INSERT INTO projects VALUES(6, 20.00, 4, 4),
+INSERT INTO projects VALUES(7, 40.00, 5, 10),
+INSERT INTO projects VALUES(8, 60.00, 6, 10),
+INSERT INTO projects VALUES(9, 50.00, 7, 9),
+INSERT INTO projects VALUES(10, 700.00, 8, 8)
