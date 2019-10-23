17c8cc846d9c3a867619a78f8893db929ecc1a18
Joseph Sesti <sestijoey63@gmail.com>
2019-10-23 19:05:13 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO users VALUES (1, 'Finnebar', 17);
+INSERT INTO users VALUES (2, 'Bear', 6);
+INSERT INTO users VALUES (3, 'Iguana', 4);
+INSERT INTO users VALUES (4, 'Alex', 33);
+INSERT INTO users VALUES (5, 'Amanda', 24);
+INSERT INTO users VALUES (6, 'Sophie', 24);
+INSERT INTO users VALUES (7, 'Rosey', 9);
+INSERT INTO users VALUES (8, 'Victoria', 23);
+INSERT INTO users VALUES (9, 'Franz', 100);
+INSERT INTO users VALUES (10, 'Hermione', 30);
+INSERT INTO users VALUES (11, 'Voldemort', 90);
+INSERT INTO users VALUES (12, 'Marisa', 24);
+INSERT INTO users VALUES 
